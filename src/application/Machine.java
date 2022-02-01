9dfbaef92f2e3d4b72cf2abe9fc9c97f729b132f
earl-stephens <stephensearl@hotmail.com>
2022-02-01 00:13:22 -0700

    Demonstrate default methods in interfaces

--- src/application/Machine.java
+++ src/application/Machine.java
@@ -1,5 +1,16 @@
 package application;
 
-public class Machine {
+public class Machine implements DefaultRunnable {
 
+	@Override
+	public void run() {
+		System.out.println("machine running");
+		
+	}
+	/* Can delete this now that it is a default
+	 * in the interface
+	public void displayDetails() {
+		System.out.println("I am a machine");
+	}
+	*/
 }
